9545bd152a994ca4de99dce5a0f00a4f40f1affb
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-08-01 19:26:14 +0300

    fixes props description of image gallery item component

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -3,7 +3,7 @@ import s from "./ImageGalleryItem.module.css";
 import PropTypes from "prop-types";
 
 const ImageGalleryItem = ({ image, largeImageURL, onModalOpen }) => {
-  // console.log(onModalOpen);
+  // console.log(image);
   const { webformatURL, tags } = image;
 
   return (
@@ -23,7 +23,11 @@ const ImageGalleryItem = ({ image, largeImageURL, onModalOpen }) => {
 };
 
 ImageGalleryItem.propTypes = {
-  image: PropTypes.object.isRequired,
+  image: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    tags: PropTypes.string.isRequired,
+    webformatURL: PropTypes.string.isRequired,
+  }),
   largeImageURL: PropTypes.string.isRequired,
   onModalOpen: PropTypes.func.isRequired,
 };
